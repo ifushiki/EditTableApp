261fc44e976334328accbf128f3a4396ee468ea6
Ikko Fushiki <ifushiki@yahoo.com>
2014-06-07 00:07:34 -0700

    Initial Check in

---
+++ EditTableApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EditTableApp
+//
+//  Created by IKKO FUSHIKI on 6/7/14.
+//  Copyright (c) 2014 IKKO FUSHIKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EditTableApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EditTableApp
+//
+//  Created by IKKO FUSHIKI on 6/7/14.
+//  Copyright (c) 2014 IKKO FUSHIKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
